8f30cf76c1f786cabca0815eb6c0c127e49262ce
Thomas2710 <thomas.trevisan@studenti.unitn.it>
2022-05-09 22:55:29 +0200

    Aggiunta connessione al db con relativi dati

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Imports
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Express app
 const app = express();
@@ -11,7 +13,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Start server
-app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
-  console.log(`Scroccotour server started`)
-})
+
+
+app.locals.db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    // Start server
+    app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
+      console.log(`Scroccotour server started`)
+    });
+});
