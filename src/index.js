797a22a62863bdbac85a37656d52cc92101fc1d8
Jiarre <38040300+Jiarre@users.noreply.github.com>
2022-05-13 01:22:20 +0200

    Added jwt support

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const login = require("./routes/login.js")
+const auth = require("./routes/auth.js")
 // Express app
 const app = express();
 app.use(cors());
@@ -18,7 +18,7 @@ app.locals.db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true
     const app = express()
 		app.use(express.urlencoded())
     app.use(express.json())
-		app.use("/login", login)
+		app.use("/api/v1/auth", auth)
     app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
       console.log(`Scroccotour server started`)
     });
--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const user = mongoose.Schema({
 	username: { type: String, unique: true },
 	password: String,
+	email: {type: String, unique: true}
 })
 
 module.exports = mongoose.model("User", user);
