5cb5d66af102f3958498873e38823cd2b0d32e13
Jiarre <38040300+Jiarre@users.noreply.github.com>
2022-05-21 19:02:02 +0200

    Code commented

--- src/routes/listahosting.js
+++ src/routes/listahosting.js
@@ -1,18 +1,28 @@
+///
+/// This files contains the backend APIs regarding functions that allow to perform queryies to retrieve Home documents
+///
+
 const express = require("express")
 const Home = require("../models/Home") // new
 const router = express.Router()
 const jwt = require('jsonwebtoken'); 
 
+//Route that retrieves all the homes that belongs to the logged user.
+//Returns the retrieved documents
 router.get("/myalloggi", async (req, res) => {
     const alloggi = await Home.find({ host: req.User.user.username })
     res.send(alloggi)
 })
-
+//Function that retrieves a specific home given its id
+//Returns the retrieved document
 router.get("/dettaglio", async (req, res) => {
     const alloggio = await Home.findById( req.query.id);
     res.send(alloggio);
 })
 
+//Function that retrieves all the homes in a specific city. 
+//If tags or/and date of start/end of availability of the home is given in the body they are also accounted for the query 
+//Returns the retrieved documents
 router.post("/alloggi", async (req, res) => {
     r = {city: req.body.city}
     console.log(req.body.tags)
