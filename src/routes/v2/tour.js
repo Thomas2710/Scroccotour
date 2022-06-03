80166ca5362556fe2b177c5eb620afe641d9c908
ciwines <emanuelecivini11@gmail.com>
2022-06-03 11:05:41 +0200

    Fix di problemi riscontrati nei test, aggiorna i codici di errore a 400 invece di 404

--- src/routes/v2/tour.js
+++ src/routes/v2/tour.js
@@ -127,8 +127,9 @@ router.post("/newtour", async (req, res) => {
 //Returns the Tour object
 router.post('/save', async (req, res) => {
     if (! req.body.tour) {
-        res.status(404);
+        res.status(400);
         res.json({success: false, message: 'Parametro tour mancante'});
+        return;
     }
 
     const updatedTour = JSON.parse(req.body.tour);
@@ -140,8 +141,9 @@ router.post('/save', async (req, res) => {
 //Returns the updated Tour object
 router.post('/book', async (req, res) => {
     if (! req.body.tour) {
-        res.status(404);
+        res.status(400);
         res.json({success: false, message: 'Parametro tour mancante'});
+        return;
     }
 
     const guest = req.User.user.username;
@@ -155,14 +157,23 @@ router.post('/book', async (req, res) => {
 //Returns the tour object
 router.post('/copy', async (req, res) => {
     if (! req.body.tourId) {
-        res.status(404);
+        res.status(400);
         res.json({success: false, message: 'Parametro tourId mancante'});
+        return;
     }
 
-    const targetTour = await Tour.findById(tourId);
-
+    let targetTour;
+    try {
+        targetTour = await Tour.findById(req.body.tourId);
+    } catch (error) {
+        console.log(error);
+        res.status(400);
+        res.json({success: false, message: 'Tour inesistente'});
+        return;
+    }
+    
     let dates = [];
-    for (let i = 0; i < randomtour.cities.length; i++) {
+    for (let i = 0; i < targetTour.cities.length; i++) {
         dates.push({
             guest: "",
             start: 0,
@@ -170,7 +181,7 @@ router.post('/copy', async (req, res) => {
         })
     }
 
-    const newTour = await Tour({
+    const copied = await Tour({
         name: "Tour",
         owner: req.User.user.username,
         start: req.body.start,
@@ -182,17 +193,17 @@ router.post('/copy', async (req, res) => {
         likes: 0,
         completed: 0,
         booked: 0,
-        nights_remaining: (req.body.end - req.body.start)/(60*60*24)
+        nights_remaining: (targetTour.end - targetTour.start)/(60*60*24)
     })
 
-
-    res.send(newTour)
+    const tour = await copied.save();
+    res.send(tour)
 })
 //Route that modifies a tour
 //Returns the tour object
 router.post('/edit', async (req, res) => {
     if (! req.body.tourId) {
-        res.status(404);
+        res.status(400);
         res.json({success: false, message: 'Parametro tourId mancante'});
     }
 
@@ -253,7 +264,7 @@ router.get("/getTour", async (req, res) => {
     else{
         const tour = await Tour.findById( req.query.id);
         if(tour == null){
-            res.status(404)
+            res.status(400)
             res.json({success: false, message: "Tour non trovato"})  
         }
         else{
